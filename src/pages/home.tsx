dc57d2b0c28d57c597e09fe1130d5ca10d91fadc
grandtan <49708254+grandtan@users.noreply.github.com>
2022-12-04 10:16:46 +0700

    build: icon lib

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View, AppRegistry } from "react-native";
+import { StyleSheet, Text, View, AppRegistry, Button, TouchableOpacity } from "react-native";
 
-const Home = () => <Text>xxxxx</Text>;
+const Home = () => {
+
+    return (
+        <View style={{ padding: 4 }}>
+            <TouchableOpacity>
+                <View style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                    <Text>Folder sample</Text>
+                    <Text>x</Text>
+                </View>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
 
 export default Home
