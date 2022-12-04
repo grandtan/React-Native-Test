68c3b7eceaffd6cea0157586f321fc3a5b3c8be6
grandtan <49708254+grandtan@users.noreply.github.com>
2022-12-04 09:19:30 +0700

    git commit -m "first commit"

---
+++ src/pages/templateDetails.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+
+export const TemplateDetails = () => <Text>TemplateDetails</Text>;
+
---
+++ src/pages/templatesList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+
+export const TemplatesList = () => <Text>TemplatesList</Text>;
+
